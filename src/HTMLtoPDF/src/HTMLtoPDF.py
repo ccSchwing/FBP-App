b99f026aabb0c5dc72eb864946f685236402abdc
Chuck Schwing <chuckschwing@proton.me>
2026-08-02 03:09:09 -0400

    Saving to 'pdfs/output.pdf' This is a temp solution.

--- src/HTMLtoPDF/src/HTMLtoPDF.py
+++ src/HTMLtoPDF/src/HTMLtoPDF.py
@@ -7,15 +7,18 @@ from weasyprint import HTML
 s3_client = boto3.client('s3')
 BUCKET_NAME = os.environ.get('S3BucketName', 'my-fbp.com')
 CLOUDFRONT_DOMAIN = os.environ.get('CloudFrontDomain')
+PDF_DIR="pdfs"
 
 def lambda_handler(event, context):
     # Get HTML content from the event body
     body = json.loads(event.get('body', '{}'))
     html_content = body.get('html', '<h1>Sorry, Dave, I\'m afraid I can\'t do that.</h1>')
-    output_key = body.get('filename', 'output.pdf')
+    output_key = body.get('filename', f'output.pdf')
+    destination= f'{PDF_DIR}/{output_key}' 
 
     # Write PDF to /tmp (Lambda's writable directory)
     tmp_pdf_path = f'/tmp/{output_key}'
+
 
     # Convert HTML to PDF using WeasyPrint
     HTML(string=html_content).write_pdf(tmp_pdf_path)
@@ -24,17 +27,17 @@ def lambda_handler(event, context):
     s3_client.upload_file(
         tmp_pdf_path,
         BUCKET_NAME,
-        output_key,
+        Key=destination,
         ExtraArgs={'ContentType': 'application/pdf'}
     )
 
-    download_url = f'https://{CLOUDFRONT_DOMAIN}/{output_key}'
+    download_url = f'https://{CLOUDFRONT_DOMAIN}/{destination}'
 
     return {
         'statusCode': 200,
         'body': json.dumps({
             'message': 'PDF generated successfully!',
             'download_url': download_url,
-            's3_key': output_key
+            's3_key': destination
         })
     }
